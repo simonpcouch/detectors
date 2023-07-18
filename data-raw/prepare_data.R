f66a044db6042473035b01ec9a214fa34c62afab
simonpcouch <simonpatrickcouch@gmail.com>
2023-07-18 09:16:42 -0500

    transform outcome to factor

--- data-raw/prepare_data.R
+++ data-raw/prepare_data.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(jsonlite)
 
-model_preds_files <- 
+model_preds_files <-
   list.files("data-raw/Data_and_Results", full.names = TRUE, recursive = TRUE)
 
 model_preds_raw <-
@@ -15,13 +15,13 @@ model_preds <-
   filter(!grepl("data\\.json|name\\.json", path)) %>%
   rowwise() %>%
   mutate(contents = list(tibble(contents)),
-         dir = gsub(basename(path), "", path)) 
+         dir = gsub(basename(path), "", path))
 
 model_metadata <-
   model_preds_raw %>%
-  filter(grepl("name\\.json", path)) %>% 
+  filter(grepl("name\\.json", path)) %>%
   rowwise() %>%
-  mutate(contents = list(enframe(contents))) %>% 
+  mutate(contents = list(enframe(contents))) %>%
   unnest(contents) %>%
   mutate(value = unlist(value)) %>%
   pivot_wider(id_cols = path, names_from = name, values_from = value) %>%
@@ -34,7 +34,7 @@ model_input <-
 
 set.seed(2023)
 
-detectors <- 
+detectors <-
   full_join(model_preds, model_metadata, "dir") %>%
   mutate(contents = list(select(contents, document, score))) %>%
   unnest(contents) %>%
@@ -54,7 +54,9 @@ detectors <-
   rename(.pred_AI = score) %>%
   mutate(
     kind = if_else(kind == "Human-Written", "Human", "AI", NA_character_),
+    kind = factor(kind, levels = c("AI", "Human")),
     .pred_class = if_else(.pred_AI > .5, "AI", "Human"),
+    .pred_class = factor(kind, levels = c("AI", "Human")),
     native = case_when(
       name %in% c("Real TOEFL") ~ "No",
       name %in% c("US 8th grade essay", "Real College Essays",
